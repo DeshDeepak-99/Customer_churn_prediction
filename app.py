32858067d4953f80f197181da28c4e8eb6747227
DeshDeepak-99 <72090122+DeshDeepak-99@users.noreply.github.com>
2020-12-14 01:16:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ import sklearn
 import matplotlib
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('Customer_Churn_Prediction.pkl', 'rb'))
+model = pickle.load(open('DeshDeepak-99
+/
+Customer_churn_prediction
+Customer_Churn_Prediction.pkl', 'rb'))
 @app.route('/', methods=['GET'])
 def Home():
     return render_template('index.html')
